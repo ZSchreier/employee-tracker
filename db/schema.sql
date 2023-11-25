39a19d942882758122db043c5852ab339481210a
zschreier <zjschreier@gmail.com>
2023-11-25 17:13:35 -0600

    set up the schema and seeds files and cleaned up the server for future work

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,28 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE movies_db;
+USE employee_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  review TEXT,
-  movie_id INT,
-  FOREIGN KEY (movie_id)
-	REFERENCES movies(id)
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  FOREIGN KEY (department_id)
+	REFERENCES department(id)
   ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  FOREIGN KEY (roles_id)
+	REFERENCES roles(id)
+  ON DELETE CASCADE,
+  manager_id INT
 );
